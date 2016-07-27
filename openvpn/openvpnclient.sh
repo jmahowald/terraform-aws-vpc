7bd46fd052c1e94612fe55ca2ff6b004a910d36a
Josh Mahowald <mahowald@gmail.com>
2016-07-27 11:44:34 -0500

    add in docker helpers for client

---
+++ openvpn/openvpnclient.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CONFIG=$1
+
+#sudo cp /path/to/vpn.crt /some/path/vpn-ca.crt
+sudo docker run --net host -it --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun \
+   -v /tmp/client.ovpn:/vpn/client.ovpn openvpn openvpn --dev /dev/net/tun  \
+   --config /vpn/client.ovpn
