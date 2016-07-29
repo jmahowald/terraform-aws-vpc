c317959cac3f0a219fcab5fabcb7433045763eb3
Josh Mahowald <mahowald@gmail.com>
2016-07-29 17:00:11 -0500

    Have peering test self containted

--- modules/peering/tests/peertest.tf
+++ modules/peering/tests/peertest.tf
@@ -1,13 +1,37 @@
-module "vpc_test" {
-  source = "../../../tests"
 
+
+variable "owner_id" {
+  default = "AAAAA"
 }
-module "peering" {
+
+module "vpc_1" {
+  source = "../../../tests"
+  vpc_cidr = "10.40.0.0/16"
+  public_subnet_cidr = "10.40.1.0/24"
+  private_subnet_cidr = "10.40.2.0/24"
+}
+
+module "vpc_2" {
+  source = "../../../tests"
+  vpc_cidr =  "192.168.0.0/16"
+  public_subnet_cidr = "192.168.1.0/24"
+  private_subnet_cidr = "192.168.2.0/24"
+}
+module "peering_1" {
     source = "../"
-    peer_vpc_id = "vpc-9ec493fa"
-    peer_owner_id = "667434029862"
+    peer_vpc_id = "${module.vpc_2.vpc_id}"
+    peer_owner_id = "${var.owner_id}"
     peer_cidr = "192.168.0.0/16"
-    vpc_id = "${module.vpc_test.vpc_id}"
+    vpc_id = "${module.vpc_1.vpc_id}"
+}
+
+
+module "peering_2" {
+    source = "../"
+    peer_vpc_id = "${module.vpc_1.vpc_id}"
+    peer_owner_id = "${var.owner_id}"
+    peer_cidr = "10.40.0.0/16"
+    vpc_id = "${module.vpc_2.vpc_id}"
 }
 /*
 variable "peer_vpc_id" {}
