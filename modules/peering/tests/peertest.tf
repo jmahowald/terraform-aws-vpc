936a05683ac8c21cadab332510706125b790c31f
Josh Mahowald <mahowald@gmail.com>
2016-08-03 15:35:00 -0500

    Have a test that actually kind of works

--- modules/peering/tests/peertest.tf
+++ modules/peering/tests/peertest.tf
@@ -3,19 +3,60 @@
 variable "owner_id" {
   default = "AAAAA"
 }
+variable "key_name" {
+  default = "testingdeploy"
+}
+variable "aws_availability_zone" {
+  default = "us-west-2a"
+}
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+module "keys" {
+  source = "../../keys"
+  key_dir = ".keys/"
+  key_name = "${var.key_name}"
+}
+
+module "centos" {
+  #TODO this should be core os?
+  source = "../../centos-amis"
+  version = "7"
+  region = "${var.aws_region}"
+}
+
 
 module "vpc_1" {
-  source = "../../../tests"
+  source = "../../network"
   vpc_cidr = "10.40.0.0/16"
   public_subnet_cidr = "10.40.1.0/24"
   private_subnet_cidr = "10.40.2.0/24"
+  aws_availability_zone = "${var.aws_availability_zone}"
+  key_name = "${var.key_name}"
+  ssh_keypath = "${module.keys.key_path}"
+  environment_name = "peer test"
+  image_user = "${module.centos.image_user}"
+  ami = "${module.centos.ami_id}"
+  aws_region = "${var.aws_region}"
+  owner = "Tester"
+
+
 }
 
 module "vpc_2" {
-  source = "../../../tests"
+  source = "../../network"
   vpc_cidr =  "192.168.0.0/16"
   public_subnet_cidr = "192.168.1.0/24"
   private_subnet_cidr = "192.168.2.0/24"
+  key_name = "${var.key_name}"
+  aws_availability_zone = "${var.aws_availability_zone}"
+  ssh_keypath = "${module.keys.key_path}"
+  environment_name = "peer test"
+  image_user = "${module.centos.image_user}"
+  ami = "${module.centos.ami_id}"
+  aws_region = "${var.aws_region}"
+  owner = "Tester"
 }
 module "peering_1" {
     source = "../"
@@ -33,6 +74,71 @@ module "peering_2" {
     peer_cidr = "10.40.0.0/16"
     vpc_id = "${module.vpc_2.vpc_id}"
 }
+
+
+
+
+resource "aws_instance" "test" {
+    availability_zone = "${var.aws_availability_zone}"
+    ami = "${module.centos.ami_id}"
+    subnet_id = "${module.vpc_1.private_subnet_id}"
+    instance_type = "t2.micro"
+    key_name = "${module.keys.key_name}"
+    count =  1
+    vpc_security_group_ids = [ "${module.vpc_1.security_group}" ]
+    tags {
+      Name = "testing-instance-${count.index}"
+      Owner = "TEST"
+      Environment = "testing"
+    }
+
+    connection {
+        user =  "${module.centos.image_user}"
+        agent = false
+        private_key = "${file(module.keys.key_path)}"
+        bastion_host = "${module.vpc_1.bastion_ip}"
+        bastion_user = "${module.vpc_1.bastion_user}"
+        #bastion_private_key = "${file(module.vpc.key_file)}"
+    }
+
+    provisioner "remote-exec" {
+        inline =  [
+            "echo 'hello world' > test.txt"
+        ]
+    }
+}
+
+
+resource "aws_instance" "test2" {
+    availability_zone = "${var.aws_availability_zone}"
+    ami = "${module.centos.ami_id}"
+    subnet_id = "${module.vpc_2.private_subnet_id}"
+    instance_type = "t2.micro"
+    key_name = "${module.keys.key_name}"
+    count =  1
+    vpc_security_group_ids = [ "${module.vpc_2.security_group}" ]
+    tags {
+      Name = "testing-instance-${count.index}"
+      Owner = "TEST"
+      Environment = "testing"
+    }
+
+    connection {
+        user =  "${module.centos.image_user}"
+        agent = false
+        private_key = "${file(module.keys.key_path)}"
+        bastion_host = "${module.vpc_2.bastion_ip}"
+        bastion_user = "${module.vpc_2.bastion_user}"
+        #bastion_private_key = "${file(module.vpc.key_file)}"
+    }
+
+    provisioner "remote-exec" {
+        inline =  [
+            "echo 'hello world' > test.txt"
+        ]
+    }
+}
+
 /*
 variable "peer_vpc_id" {}
 variable "peer_owner_id" {}
