a4263ff0fddaf66d7dacca35227c7fc26b6a970b
Josh Mahowald <mahowald@gmail.com>
2016-09-09 00:04:43 -0500

    added in entry for ap south 1

--- modules/centos-amis/variables.tf
+++ modules/centos-amis/variables.tf
@@ -10,6 +10,7 @@ variable "centos_amis" {
       "7-aws-us-west-1" = "ami-af4333cf"
       "7-aws-us-east-1" = "ami-6d1c2007"
       "7-aws-us-west-2" = "ami-d2c924b2"
+      "7-aws-ap-south-1" = "ami-95cda6fa"
       "7-aws-ap-southeast-2" = "ami-fedafc9d"
       "7-aws-eu-central-1" = "ami-9bf712f4"
     }
